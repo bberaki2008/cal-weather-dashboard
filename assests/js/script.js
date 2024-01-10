25a562508030ec4ee89ef7f24c6c7e0f38a6cf96
Berie <bberaki2015@gmail.com>
2024-01-09 23:44:41 -0800

    created stylesheet and cleaned redundant elements and minor edit to js file

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,26 +7,42 @@ function getApi(event) {
   event.preventDefault();
   let city =document.getElementById('city').value;
   console.log(city);
+  let api_key ="b999cb8b22b053825ee574c293c2deaa"
   // fetch request gets a list of all the repos for the node.js organization
-  // let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=37.773972&lon=-122.431297&appid=c32948bb2ac0b7689e6b43e0928bff93';
-  let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c32948bb2ac0b7689e6b43e0928bff93`;
+  // let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?lat=37.773972&lon=-122.431297&appid=b999cb8b22b053825ee574c293c2deaa';
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${api_key}&units=metric`;
+
 
 //replace space with +sign.
 
   fetch(requestUrl)
     .then(function (response) {
+      //console.log(response);
       return response.json();
     })
     .then(function (data) {
-      let windSpeed =data.wind.speed;
-      let temperature=  (data.main.temp - 273.15) * 1.8 + 32;
-      let humid = data.main.humidity;
-      let weatherIcon = data.; ///
-      let date = data //
-      console.log(windSpeed);
-      console.log(temperature);
-      console.log(humid);
+      // for(let i = 0; i<data.length; i++){
+      //   let windSpeed =data.wind.speed;
+      //   let temperature=  (data.main.temp) //- 273.15) * 1.8 + 32;
+      //   let humid = data.main.humidity;
+      //   let weatherIcon = data.weather['0'].icon;
+      //   let date = moment.unix(data.dt).format("M/DD/YYYY");
+      //   document.getElementById('dateDay1').textContent = date;
+      //   document.getElementById('icon1').textContent = weatherIcon;
+      //   document.getElementById('temp1').textContent = temperature;
+      //   document.getElementById('wind1').textContent = windSpeed;
+      //   document.getElementById('humid1').textContent = humid;
+
+      // }
+
+
+      // console.log(windSpeed);
+      // console.log(temperature);
+      // console.log(humid);
+      // console.log(weatherIcon);
+      // console.log(date);
       console.log(data);
+
       //Loop over the data to generate a table, each table row will have a link to the repo url
       // for (var i = 0; i < data.length; i++) {
       //   // Creating elements, tablerow, tabledata, and anchor
